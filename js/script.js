b9dd58d151c53ab073edab95d3b7d32a3ef9bad7
Interno <xurshidabdujabb@gmail.com>
2022-10-30 22:04:57 +0300

    NFT me layout

---
+++ js/script.js
@@ -0,0 +1,71 @@
+window.addEventListener("load", function (e) {
+    // header navbar menu toggle
+    $("header .activeBars").click(function () {
+      $("header .activeMenuBars").slideToggle(1000);
+    });
+    // headerLogin
+    $("header .eyes").click(function () {
+        var attr = $("header .pass input").attr("type");
+        if (attr == "password") {
+          $("header .pass input").attr("type", "text");
+        } else {
+          $("header .pass input").attr("type", "password");
+        }
+      });
+    
+      // header navbar signin form error Validation togirlash kerak
+    
+      $(".headerLogin button").click(function (e) {
+        e.preventDefault();
+        if ($(".headerLogin input").val() === "") {
+          $(".errorMsg").show();
+        } else {
+          $(".errorMsg").hide();
+        }
+      });
+    
+      /// header navbar signin close
+    
+      $(".headerLogin .close").click(function () {
+        $(".headerLogin").fadeOut(500);
+      });
+    
+      $("header .signUp").click(function () {
+        $(".headerLogin").fadeIn();
+        $(".headerLogin").css("display", "flex");
+      });
+    //  main
+    let time = 3600;
+    const countDownEl = document.getElementById
+      ("countdown");
+    const countEl = document.getElementById  
+    ("count")
+    const lilEl = this.document.getElementById
+    ("lil")
+  
+    setInterval(updatecountdown, 1000);
+  
+    function updatecountdown() {
+      const minutes = Math.floor(time / 60)
+      let seconds = time % 60;
+      seconds = seconds < 10 ? "0" + seconds :
+        seconds;
+      countDownEl.innerHTML = `${minutes} :
+         ${seconds}`;
+      time--; 
+
+      const sec = Math.floor(time / 80)
+      let conds = time % 60;
+      conds = conds < 10 ? "0" + conds:
+      conds;
+       countEl.innerHTML = `${sec} : ${conds}`
+       time--;
+
+       const ces = Math.floor(time / 100)
+       let nec = time % 60;
+       nec = nec < 10 ? "0" + nec:
+       nec;
+       lilEl.innerHTML = `${ces} : ${nec}`
+       time--;
+      }
+});
